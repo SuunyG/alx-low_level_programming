6304e70c923233bc89451c8b907cb34e6d21d3e0
SuunyG <sundaygeorge247@gmail.com>
2022-06-30 16:39:07 -0700

    script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-/**
-* main - this is the main function
-* description - Printing the output with the put function
-* it returns zero (0)
-*/
-void main(void)
+/** main - this is the main function
+ ** description - Printing the output with the put function
+ ** it returns zero (0)
+**/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
