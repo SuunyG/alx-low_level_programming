f9d4f03554c2e93bd8ce9ccc07a2e823a7751438
SuunyG <sundaygeorge247@gmail.com>
2022-06-30 15:45:35 -0700

    put functions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ *Printing the output with the put function
+ *it returns zero (0)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
