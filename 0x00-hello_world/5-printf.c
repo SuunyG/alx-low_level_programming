7efa1436881c314085eeeddd80841458f2037206
SuunyG <sundaygeorge247@gmail.com>
2022-07-01 00:54:05 -0700

    ssisizes of varriable type

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-/*
-* main - Entry point
-* Return: Always 0 (success)
-*/
+
+/**
+ * main - Entry point
+ * Description: using printf()
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
