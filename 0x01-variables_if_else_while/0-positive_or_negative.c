6ce63f7820f507103461cefbd7293c77f51c5eac
SuunyG <sundaygeorge247@gmail.com>
2022-07-01 22:48:51 -0700

    positive and egative output

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints a random number
+ * provided it's positive or negative
+ * Return: Aways 0.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* The code starts from here*/
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
