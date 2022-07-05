b9eb80f3729dea6d939ea5598419a795d34f9534
SuunyG <sundaygeorge247@gmail.com>
2022-07-05 14:26:22 -0700

    c next level

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints _putchar
+ * Return: returns 1 for Successful and -1 for failed
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
