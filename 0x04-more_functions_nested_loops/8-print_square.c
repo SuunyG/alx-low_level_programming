49142865ba07a7a9c04207895e9dc3e3e4933eb0
SuunyG <sundaygeorge247@gmail.com>
2022-07-08 22:00:46 -0700

    Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_square -> printing line
- * @size: integer params
+ * * print_square -> printing line
+ * * @size: integer parameter
  */
 void print_square(int size)
 {
 	int x, y;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
 	{
 		for (x = 0; x < size; x++)
 		{
