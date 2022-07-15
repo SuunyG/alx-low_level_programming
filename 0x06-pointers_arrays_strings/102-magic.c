8ab7fe3d09909b0b9b21d38fcca3d24a089cc79a
SuunyG <sundaygeorge247@gmail.com>
2022-07-15 13:17:30 -0700

    completed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,7 +5,6 @@
  * print_number - Prints any integer with putchar
  * Return: Aways 0.
  */
-
 int main(void)
 {
 	int n;
@@ -14,14 +13,9 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-       	/*
-	 * write your line of code here..
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
+	/* write your line of code here.. */
+	/* Remember: you are not allowed to use,to modify, only one statement*/
+	/* - you are not allowed to code anything else than this line of code*/
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
