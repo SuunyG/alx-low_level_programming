e622877c4012eb2174a122a87a0abdd8af9f005c
SuunyG <sundaygeorge247@gmail.com>
2022-07-14 17:57:48 -0700

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
 /**
- * *_strcatchar - function that copy
+ * _strcat - function that copy
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
  * Return: Always return 0.
  */
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
-	char *dest[98] = "Hello";
-	char *src[98] = "World!\n";
-	char *ptr;
+	int s1 = 0, s2 = 0;
 
-	_strcat(dest, src);
-	_putchar("_strcat( dest, src):   %s\n", dest);
+	while (dest[s1++])
+		s2++;
+
+	for (s1 = 0; src[s1]; s1++)
+		dest[s2++] = src[s1];
+
+	return (dest);
 }
