50baa5eb902e220112a6ec98cd83db49d5822620
SuunyG <sundaygeorge247@gmail.com>
2022-07-14 19:53:25 -0700

    more on arrays and pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - comparing strings
+ * @s1: aug1
+ * @s2: aug2
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+
+	return (result);
+}
