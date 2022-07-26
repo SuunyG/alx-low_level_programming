23f284c5e73a69e8ae143749ec9d03d02a6eed3e
SuunyG <sundaygeorge247@gmail.com>
2022-07-26 13:24:12 -0700

    Print number of argument passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * @argv: an array of command line arguements
  * Return: 0 for success
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", (argc - 1));
+	printf("%d\n", argc);
 	return (0);
 }
